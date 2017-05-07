1e6683435e094cd25973a5cfe19f7d5fa51919e0
chrisbrickey <cbrickey1@gmail.com>
2017-05-07 10:41:24 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# webscraper3
+This program scrapes a URL, parses by an HTML element name, and converts the section into a csv document.
+
+
+## Technology
+Ruby version 2.4.0p0
+
+
+## Dependencies
+* gem install nokogiri
+
+
+## Setup
+* Run from terminal: `ruby breakout.rb`
+
+
+## Upcoming Features
+* Finalize logic to pull breakout list table
+* Add feature to convert to CSV document. (see webscraper1)
---
+++ breakoutlist.rb
@@ -0,0 +1,32 @@
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+
+
+#Parameters must be in string format e.g. ("http://www.testing123.com", "//outertag//innertag")
+def scrape (url_string, tag_pattern)
+
+  #scrapes entire HTML document and places it in a Nokogiri object (requires 'nokogiri' and 'open-uri')
+  scraped_object = Nokogiri::HTML(open(url_string))
+
+  #filters the scraped_object and stores it into a node set
+  #tag_patterns are ideally specified to pull multiple items (e.g. multiple <p></p> that match the same condition) which result in an array of many items
+  parsed_node_set = scraped_object.css(tag_pattern)
+
+  parsed_array = []
+  #converts nodeset to array of strings
+  parsed_node_set.each do |element|
+    parsed_array << element.to_s
+  end
+
+  parsed_array
+end
+
+
+breakoutlist_URL = "https://breakoutlist.com/"
+puts scrape(breakoutlist_URL,"//table//tr//td")
+
+
+
+#See this page for how to assign specific tags based on the URL: http://www.nokogiri.org/tutorials/searching_a_xml_html_document.html
+#doc.css('//car:tire', 'car' => 'http://alicesautoparts.com/')
