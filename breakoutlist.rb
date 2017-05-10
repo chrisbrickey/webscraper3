948d6dbfda8699901501ab6f0e0e4c8e5d75d72e
chrisbrickey <cbrickey1@gmail.com>
2017-05-10 14:09:54 -0700

    adjusted parsing, added csv export

--- breakoutlist.rb
+++ breakoutlist.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 require 'json'
+require 'csv'
 
 
 #Parameters must be in string format e.g. ("http://www.testing123.com", "//outertag//innertag")
@@ -13,18 +14,32 @@ def scrape (url_string, tag_pattern)
   #tag_patterns are ideally specified to pull multiple items (e.g. multiple <p></p> that match the same condition) which result in an array of many items
   parsed_node_set = scraped_object.css(tag_pattern)
 
-  parsed_array = []
-  #converts nodeset to array of strings
-  parsed_node_set.each do |element|
-    parsed_array << element.to_s
+  # parsed_array = []
+  # #converts nodeset to array of strings
+  # parsed_node_set.each do |element|
+  #   parsed_array << element.to_s
+  # end
+  #
+  # parsed_array
+
+
+  text_array = []
+  parsed_node_set.map do |item|
+    text_array.push(item)
   end
 
-  parsed_array
+  #push the array into the csv file
+  CSV.open('results.csv', 'w') do |csv|
+    csv << text_array
+  end
+
+
 end
 
 
 breakoutlist_URL = "https://breakoutlist.com/"
-puts scrape(breakoutlist_URL,"//table//tr//td")
+# puts scrape(breakoutlist_URL,"//table//tr//td")
+puts scrape(breakoutlist_URL,"//table//tr")
 
 
 
